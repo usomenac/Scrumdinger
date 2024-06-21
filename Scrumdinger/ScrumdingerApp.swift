be80d18128c3c972655863f7efac3cdf07f2fb19
Lorenzo Mazzarotto <usomenac@icloud.com>
2024-06-21 04:20:19 -0300

    Create a project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Lorenzo Mazzarotto on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
