8f4d93f2315f2182ef44127af31eee3b3f2fdcec
Lorenzo Mazzarotto <usomenac@icloud.com>
2024-06-22 18:22:08 -0300

    Compose views

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,32 @@
+//
+//  MeetingView.swift
+//  Scrumdinger
+//
+//  Created by Lorenzo Mazzarotto on 21/06/24.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
